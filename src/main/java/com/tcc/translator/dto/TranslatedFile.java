b133e6bb4332f9756e49834b7fb78ce6b2a295f6
Willian Alves <wba.alves@gmail.com>
2020-09-01 00:36:46 -0300

    create DTO for translated file

---
+++ src/main/java/com/tcc/translator/dto/TranslatedFile.java
@@ -0,0 +1,23 @@
+package com.tcc.translator.dto;
+
+public class TranslatedFile {
+
+  private String fileName;
+  private String content;
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
